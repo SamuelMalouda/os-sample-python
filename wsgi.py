78c4d8fbde909e2a4d99c2573acd9ae527ab2a8a
SamuelMalouda <samuel.malouda@live.fr>
2017-08-28 21:17:54 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Ta grand mere la pute OS"
 
 if __name__ == "__main__":
     application.run()
